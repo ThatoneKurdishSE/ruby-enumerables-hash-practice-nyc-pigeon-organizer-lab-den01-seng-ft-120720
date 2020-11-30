c183eb0de92b2d47cc760e0c8ca2fa2bb838c0d9
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-30 02:57:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   betterData= hash.new
-  data[:gender].each do |key, array|
+data[:gender].each do |key, array|
     array.each do |name|
       betterData[:"#{name}"] = {:gender => ["#{key}"], :color=> [], :lives =>[]}
+    end
+  end
+data[:color].each do |key, array|
+    array.each do |name|
+      betterData[:"#{name}"][:color]<< "#{key}"
+    end
+  end
+  data[:lives].each do |key, array|
+    array.each do |name|
+      betterData[:"#{name}"][:lives]<<"#{key}"
     end
   end
